d5715a4e2824d2d90f4058ff901ad63d3d94ab96
*vivek <vrvivekofficial@gmail.com>
2025-03-27 12:23:31 +0530

    added solution for search in rotated sorted array

--- Search in Rotated Sorted Array/solution.js
+++ Search in Rotated Sorted Array/solution.js
@@ -1,25 +1,30 @@
 var search = function (nums, target) {
-  let left = 0
-  let right = nums.length - 1
+  let left = 0;
+  let right = nums.length - 1;
 
   while (left <= right) {
-      let mid = Math.floor((left + right) / 2)
+    let mid = Math.floor((left + right) / 2);
 
-      if (nums[mid] === target) {
-          return mid
-      } else if (nums[mid] >= nums[left]) {
-          if (nums[left] <= target && target <= nums[mid]) {
-              right = mid - 1
-          } else {
-              left = mid + 1
-          }
+    if (nums[mid] === target) {
+      return mid;
+    }
+
+    // Determine if left half is sorted
+    if (nums[left] <= nums[mid]) {
+      if (nums[left] <= target && target < nums[mid]) {
+        right = mid - 1;
       } else {
-          if (nums[right] <= target && target <= nums[mid]) {
-              left = mid + 1
-          } else {
-              right = mid - 1
-          }
+        left = mid + 1;
       }
+    } 
+    // Right half must be sorted
+    else {
+      if (nums[mid] < target && target <= nums[right]) {
+        left = mid + 1;
+      } else {
+        right = mid - 1;
+      }
+    }
   }
-  return -1
+  return -1;
 };
