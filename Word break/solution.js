808b4d8a0babe1a40c15fd9f517296549e9a845c
*vivek <vrvivekofficial@gmail.com>
2025-04-11 15:58:00 +0530

    added soluton for word break

---
+++ Word break/solution.js
@@ -0,0 +1,20 @@
+var wordBreak = function(s, wordDict) {
+  let dp = new Array(s.length + 1).fill(false);
+  dp[0] = true;
+
+  for (let i = 1; i <= s.length; i++) {
+      for (let w of wordDict) {
+        let start = i - w.length;        
+          if (start >= 0 && dp[start] && s.substring(start, i) === w) {
+              dp[i] = true;
+              break;
+          }
+      }
+  }
+  return dp[s.length];    
+};
+
+let s = "catsandog"
+let  wordDict = ["cats", "dog", "sand", "and", "cat"]
+
+wordBreak(s,wordDict)
