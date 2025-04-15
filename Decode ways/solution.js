4b0bd941e7a7f866c07d046b1baf2cd9fd591b62
*vivek <vrvivekofficial@gmail.com>
2025-04-15 15:58:14 +0530

    added solution for decode ways

---
+++ Decode ways/solution.js
@@ -0,0 +1,23 @@
+var numDecodings = function(s) {
+  if (s[0] === '0') {
+      return 0;
+  }
+
+  const n = s.length;
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = dp[1] = 1;
+
+  for (let i = 2; i <= n; i++) {
+      const one = parseInt(s[i - 1]);
+      const two = parseInt(s.substring(i - 2, i));
+
+      if (1 <= one && one <= 9) {
+          dp[i] += dp[i - 1];
+      }
+      if (10 <= two && two <= 26) {
+          dp[i] += dp[i - 2];
+      }
+  }
+
+  return dp[n];    
+};
