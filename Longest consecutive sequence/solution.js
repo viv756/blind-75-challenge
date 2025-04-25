6fb364d58a97fdae139bdb87f9e047f5e92841b2
*vivek <vrvivekofficial@gmail.com>
2025-04-25 10:46:50 +0530

    added solution for longest consecutive sequence

---
+++ Longest consecutive sequence/solution.js
@@ -0,0 +1,18 @@
+var longestConsecutive = function(nums) {
+  const numSet = new Set(nums);
+  let longest = 0;
+
+  for (let n of numSet) {
+      if (!numSet.has(n - 1)) {
+          let length = 1;
+
+          while (numSet.has(n + length)) {
+              length++;
+          }
+
+          longest = Math.max(longest, length);
+      }
+  }
+
+  return longest;    
+};
